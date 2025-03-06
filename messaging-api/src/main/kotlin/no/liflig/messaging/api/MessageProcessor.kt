ac94c854a576125c6cb84e4a1b6feeeb75e9897d
hermannm <hermann.morkrid@gmail.com>
2025-03-06 12:39:59 +0100

    Add severity field to ProcessingResult.Failure

--- messaging-api/src/main/kotlin/no/liflig/messaging/api/MessageProcessor.kt
+++ messaging-api/src/main/kotlin/no/liflig/messaging/api/MessageProcessor.kt
@@ -1,11 +1,13 @@
 package no.liflig.messaging.api
 
+import no.liflig.logging.LogLevel
+
 /**
  * Interface for handling messages received from a queue.
  *
  * You implement your custom message processing with this interface. Then, construct a
  * [MessagePoller] using your `MessageProcessor` implementation and a
- * [Queue][no.liflig.messaging.queue.Queue]. The message poller will then poll messages from the
+ * [Queue][no.liflig.messaging.api.queue.Queue]. The message poller will then poll messages from the
  * queue service and call your message processor with each message.
  */
 public interface MessageProcessor {
@@ -23,5 +25,13 @@ public sealed class ProcessingResult {
        * logs.
        */
       val cause: Throwable? = null,
+      /**
+       * The severity of the failure.
+       *
+       * [DefaultMessagePollerObserver.onMessageFailure] uses this as the log level when logging a
+       * message processing failure. By default, the ERROR log level is used, but you may want to
+       * lower this if it represents an expected failure.
+       */
+      val severity: LogLevel = LogLevel.ERROR,
   ) : ProcessingResult()
 }
